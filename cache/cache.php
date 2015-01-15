78f48969afc411b5389a349141893a1d88d43c8e
nicobascon <nico.bascon@live.fr>
2015-01-15 15:14:46 +0100

    Add cache

---
+++ cache/cache.php
@@ -0,0 +1,11 @@
+<?php
+
+$cache_map = array (
+  'TestCase' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\tests\\TestCase.php',
+  'App' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\App.php',
+  'View\\TemplateEngineInterface' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\View\\TemplateEngineInterface.php',
+  'Exception\\ExceptionHandler' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\Exception\\ExceptionHandler.php',
+  'Routing\\Route' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\Routing\\Route.php',
+  'Exception\\HttpException' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\Exception\\HttpException.php',
+  'View\\TemplateEngine' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\View\\TemplateEngine.php',
+);
