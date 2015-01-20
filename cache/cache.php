742e27c9bc0c512ca63f337f2822553cdfa96978
nicobascon <nico.bascon@live.fr>
2015-01-20 16:08:30 +0100

    Update cache

--- cache/cache.php
+++ cache/cache.php
@@ -1,11 +1,11 @@
 <?php
 
 $cache_map = array (
-  'TestCase' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\tests\\TestCase.php',
-  'App' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\App.php',
-  'View\\TemplateEngineInterface' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\View\\TemplateEngineInterface.php',
-  'Exception\\ExceptionHandler' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\Exception\\ExceptionHandler.php',
-  'Routing\\Route' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\Routing\\Route.php',
-  'Exception\\HttpException' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\Exception\\HttpException.php',
-  'View\\TemplateEngine' => 'C:\\Users\\Nicolas\\Documents\\GitHub\\YearLicence2\\projects\\uframework\\src\\View\\TemplateEngine.php',
+  'TestCase' => '/var/www/uframework/tests/TestCase.php',
+  'App' => '/var/www/uframework/src/App.php',
+  'View\\TemplateEngineInterface' => '/var/www/uframework/src/View/TemplateEngineInterface.php',
+  'Exception\\ExceptionHandler' => '/var/www/uframework/src/Exception/ExceptionHandler.php',
+  'Routing\\Route' => '/var/www/uframework/src/Routing/Route.php',
+  'Exception\\HttpException' => '/var/www/uframework/src/Exception/HttpException.php',
+  'View\\TemplateEngine' => '/var/www/uframework/src/View/TemplateEngine.php',
 );
