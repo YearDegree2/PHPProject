89b53b3c11c205c0cc64de189e4968cb4242b928
nicobascon <nico.bascon@live.fr>
2015-01-20 16:39:55 +0100

    Add Status class

---
+++ src/Model/Status.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Model;
+
+class Status
+{
+    private $message;
+    private $id;
+    private $username;
+    private $date;
+    private $clientUsed;
+
+    public function __construct($message, $id, $username, $date, $clientUsed = "PC")
+    {
+        $this->message      = $message;
+        $this->id           = $id;
+        $this->username     = $username;
+        $this->date         = $date;
+        $this->clientUsed   = $clientUsed;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function getClientUsed()
+    {
+        return $this->clientUsed;
+    }
+}
