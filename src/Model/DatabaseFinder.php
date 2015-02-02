a448305f274ae1d0224d8a926b3244ffea7d1df3
nicobascon <nico.bascon@live.fr>
2015-02-03 00:03:05 +0100

    Add data mapper

--- src/Model/DatabaseFinder.php
+++ src/Model/DatabaseFinder.php
@@ -6,7 +6,7 @@ class DatabaseFinder implements FinderInterface
 {
     private $connection;
 
-    public function __construct($connection)
+    public function __construct(Connection $connection)
     {
         $this->connection = $connection;
     }
@@ -34,24 +34,5 @@ class DatabaseFinder implements FinderInterface
         $result = $result->fetch(\PDO::FETCH_ASSOC);
 
         return ($result !== false) ? new Status($result['content'], $result['id'], $result['username'], new \DateTime($result['date']), $result['clientused']) : null;
-    }
-
-    public function addStatus(Status $status)
-    {
-        $query = "INSERT INTO statuses (username, content, date, clientused) VALUES (:username, :message, :date, :clientused)";
-        $parameters = [
-            ':username'     => $status->getUsername(),
-            ':message'      => $status->getMessage(),
-            ':date'         => $status->getDate(),
-            ':clientused'   => $status->getClientUsed(),
-        ];
-        $this->connection->executeQuery($query, $parameters);
-    }
-
-    public function deleteStatus(Status $status)
-    {
-        $query = "DELETE FROM statuses WHERE id = :id";
-        $parameters = [':id' => $status->getId()];
-        $this->connection->executeQuery($query, $parameters);
     }
 }
---
+++ src/Model/StatusDataMapper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Model;
+
+class StatusDataMapper implements DataMapperInterface
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function persist(Status $status)
+    {
+        // Status length max : 140 characters
+        if (mb_strlen($status->getMessage()) > 140) {
+            return null;
+        }
+        $query = "INSERT INTO statuses (username, content, date, clientused) VALUES (:username, :message, :date, :clientused)";
+        $parameters = [
+            ':username'     => $status->getUsername(),
+            ':message'      => $status->getMessage(),
+            ':date'         => $status->getDate(),
+            ':clientused'   => $status->getClientUsed(),
+        ];
+        $this->connection->executeQuery($query, $parameters);
+
+        return true;
+    }
+
+    public function remove(Status $status)
+    {
+        $query = "DELETE FROM statuses WHERE id = :id";
+        $parameters = [':id' => $status->getId()];
+        $this->connection->executeQuery($query, $parameters);
+    }
+}
